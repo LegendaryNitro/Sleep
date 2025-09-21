0afcb67c8d833ac83846c36b14f6ce0c5eb33c71
Arodistribution <vumaniveejay@gmail.com>
2025-09-21 19:45:15 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/theme-provider";
 import Index from "/Index";
-import NotFound from "/pages";
+import pages from "/pages";
 
 const queryClient = new QueryClient();
 
